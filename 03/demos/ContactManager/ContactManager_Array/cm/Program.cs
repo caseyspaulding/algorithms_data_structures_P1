68a7032cced8862285975a4f8a7b8d20e8bbfacd
Casey Spaulding <casey.spaulding@me.com>
2023-01-26 09:14:05 -0500

    init

---
+++ 03/demos/ContactManager/ContactManager_Array/cm/Program.cs
@@ -0,0 +1,16 @@
+﻿using ContactManager;
+using System;
+
+namespace cm
+{
+    class Program
+    {
+        static void Main()
+        {
+            ContactStore store = new ContactStore();
+            Repl repl = new Repl(Console.In, Console.Out, store);
+
+            repl.Run();
+        }
+    }
+}
